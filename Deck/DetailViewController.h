4fa5cdb51dbfca99f0c717570a49b9471a22de50
Ewan Leaver <Ewan@Ewans-MacBook-Pro.local>
2014-04-21 22:24:03 +0900

    Initial Commit

---
+++ Deck/DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  Deck
+//
+//  Created by Ewan Leaver on 21/04/2014.
+//  Copyright (c) 2014 Ewan Leaver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController <UISplitViewControllerDelegate>
+
+@property (strong, nonatomic) id detailItem;
+
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@end
